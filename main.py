091e348d784aa0f145894bc03d8b672ef54e8e5e
Ameya Deo <122508209+ADeo123@users.noreply.github.com>
2024-01-10 15:39:28 -0500

    Progress #1

--- main.py
+++ main.py
@@ -4,5 +4,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px 
 
-data = pd.read_csv("dataSets/temperature-anomaly.csv")
-print(data)
+# data = pd.read_csv("dataSets/temperature-anomaly.csv")
+# print(data.head)
+
+dataTwo = pd.read_csv("dataSets/co-emissions-per-capita.csv")
+print(dataTwo.head)
+
+# print("<------------------->\n")
+# print(data.describe())
+
+# print("<------------------->\n")
+# print(data.info())
+
+# print("<------------------->\n")
+# print(data.describe())
+
+##Shows the 
+# figure = px.line(data, x="Year", y="Global average temperature anomaly relative to 1961-1990", title="Global Temperature Anomalies")
+# figure.show() 
+
+figureTwo = px.line(dataTwo, x="Year", y="Annual CO₂ emissions (per capita)", title="Your Title Here")
+figureTwo.show()
+
+
+
+
+
+
