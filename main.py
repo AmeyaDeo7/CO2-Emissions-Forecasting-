ffc22cc06b00a7e9b04d4e55b58575700e6134de
Ameya Deo <122508209+ADeo123@users.noreply.github.com>
2024-01-10 19:46:05 -0500

    Progress #2

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ print(dataTwo.head)
 # figure = px.line(data, x="Year", y="Global average temperature anomaly relative to 1961-1990", title="Global Temperature Anomalies")
 # figure.show() 
 
-figureTwo = px.line(dataTwo, x="Year", y="Annual CO₂ emissions (per capita)", title="Your Title Here")
-figureTwo.show()
+# figureTwo = px.line(dataTwo, x="Year", y="Annual CO₂ emissions (per capita)", title="Global C02 Emissions")
+# figureTwo.show()
 
+figureTwo = px.bar(dataTwo, x="Entity", y="Annual CO₂ emissions (per capita)", title="Average C02 Emissions Per Capita")
+figureTwo.show()
 
 
 
